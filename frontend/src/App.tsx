58cc8579edab947710f682dcc1eacb42c7a92d70
Birdingcode <filbert.goh@gmail.com>
2025-03-03 21:04:13 +0800

    import changes and .env gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import AppRoutes from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <MantineProvider>
-      <AppRoutes />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <AppRoutes />
+      </MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
