3f14700655ac5f60f74a920641f7486a7f3ab4c3
Birdingcode <filbert.goh@gmail.com>
2025-03-18 20:01:57 +0800

    wip: react hook form login page & removing tanstack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import AppRoutes from "./routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider>
-        <AppRoutes />
-      </MantineProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <MantineProvider>
+      <AppRoutes />
+    </MantineProvider>
   );
 }
 
--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -11,39 +11,85 @@ import {
   Title,
 } from "@mantine/core";
 import classes from "./login.module.css";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import axios from "axios";
+
+interface IFormInput {
+  username: string;
+  password: string;
+}
 
 const LoginPage: React.FC = () => {
-  return (
-    <Container size={420} my={40}>
-      <Title ta="center" className={classes.title}>
-        Welcome back!
-      </Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" component="button">
-          Create account
-        </Anchor>
-      </Text>
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <TextInput label="Email" placeholder="test@email.com" required />
-        <PasswordInput
-          label="Password"
-          placeholder="Your password"
-          required
-          mt="md"
-        />
-        <Group justify="space-between" mt="lg">
-          <Checkbox label="Remember me" />
-          <Anchor component="button" size="sm">
-            Forgot password?
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_HOST}/login`,
+        data
+      );
+      console.log(response);
+    } catch (err: unknown) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container size={420} my={40}>
+        <Title ta="center" className={classes.title}>
+          Welcome back!
+        </Title>
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Anchor size="sm" component="button">
+            Create account
           </Anchor>
-        </Group>
-        <Button fullWidth mt="xl">
-          Sign in
-        </Button>
-      </Paper>
-    </Container>
+        </Text>
+
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <Controller
+            name="username"
+            control={control}
+            render={({ field }) => (
+              <TextInput
+                label="Email"
+                placeholder="test@email.com"
+                required
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <PasswordInput
+                label="Password"
+                placeholder="Your password"
+                required
+                mt="md"
+                {...field}
+              />
+            )}
+          />
+          <Group justify="space-between" mt="lg">
+            <Checkbox label="Remember me" />
+            <Anchor component="button" size="sm">
+              Forgot password?
+            </Anchor>
+          </Group>
+          <Button type="submit" fullWidth mt="xl">
+            Sign in
+          </Button>
+        </Paper>
+      </Container>
+    </form>
   );
 };
 
